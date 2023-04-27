2acd88e614c8e37afe15a0df9411bc10fca348b5
Will Macdonald <willmacdonald@users.noreply.github.com>
2023-04-27 10:22:29 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,4 +38,4 @@ with st.echo(code_location='below'):
         .encode(x='x:Q', y='y:Q'))
     
     image_url = "https://dataprofessor-st-demo-image-markdown-streamlit-app-layled.streamlit.app/~/+/media/c1d64cbe224f2a71943d37c5294f12c656d9379b6b866e3f418fe9aa.png"
-    st.markdown("![](/image_url)")
+    st.image(image_url)
