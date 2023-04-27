b16ff7580b8f6f267fb8a94c280691d1f705f08a
Will Macdonald <willmacdonald@users.noreply.github.com>
2023-04-27 15:00:30 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,20 @@
 import streamlit as st
+import os
+import openai
 
-image_url = "https://dataprofessor-st-demo-image-markdown-streamlit-app-layled.streamlit.app/~/+/media/c1d64cbe224f2a71943d37c5294f12c656d9379b6b866e3f418fe9aa.png"
-st.image(image_url)    
+topics = "Here is some bicycle terminology that is often confused, conflated, or straight up mangled. This is all in good fun, as I probably make more mistakes than the average YouTuber. It's also worth nothing that I don't care what you call anything and rarely correct anyone... except when they call a damper a dampener. That's yucky."
+
+def generate(topics):
+    openai.api_key = os.environ['OPENAI_IMAGE_API_KEY'];
+    openai.organization = "org-ZXIgSEZAnove37o1egimi8P8"
+
+    response = openai.Image.create(
+        prompt=",
+        n=1,
+        size="256x256",
+        )
+    return {
+        response["data"][0]["url"]
+    }
+  
+  st.image(generate(topics))
