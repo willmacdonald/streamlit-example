2cd866741e47b3d2e6c0b43511c4053d53f10f5c
Will Macdonald <willmacdonald@users.noreply.github.com>
2023-04-27 16:07:14 +0200

    use secrets

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import openai
 topics = "Here is some bicycle terminology that is often confused conflated or straight up mangled. This is all in good fun, as I probably make more mistakes than the average YouTuber. It's also worth nothing that I don't care what you call anything and rarely correct anyone, except when they call a damper a dampener. That is yucky."
 
 def generate(topics):
-    openai.api_key =  "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"
+    openai.api_key =  st.secrets["OPENAI_IMAGE_API_KEY"]
     openai.organization = "org-ZXIgSEZAnove37o1egimi8P8"
 
     response = openai.Image.create(
